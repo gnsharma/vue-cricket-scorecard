0571659429a2a5102d27e91cb93abf2f24d821ea
Govind Sharma <gnsharma0810@gmail.com>
2019-04-17 17:14:33 +0530

    add bowler component

---
+++ src/components/Bowler.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Bowler">
+        <p class="text-left m-3"> Bowler </p>
+        This over:
+        <span v-for="(item, index) in thisOver" v-bind:key="index">
+            {{ item }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Bowler",
+    props: {
+        thisOver: Array
+    }
+}
+</script>
+
