02aea0e946ec0cd6d8996d8d1256b6d4fe63a8e5
Govind Sharma <gnsharma0810@gmail.com>
2019-04-23 12:48:52 +0530

    add refs example code final

---
+++ src/components/Celebration.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="celebration" v-if="show">
+    Yay!!! You just hit a six. Smashed it!!!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Celebration",
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    showDiv() {
+      this.show = true;
+    },
+    hideDiv() {
+      this.show = false;
+    }
+  }
+
+};
+</script>
